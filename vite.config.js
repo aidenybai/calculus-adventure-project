55a022c9fbfe26dd485abc7bfb1580886e7a9910
Aiden Bai <aiden.bai05@gmail.com>
2022-05-17 14:05:27 -0700

    init

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
-export default {
-  plugins: ['@vitejs/plugin-legacy'],
-};
+import legacy from '@vitejs/plugin-legacy';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+});
